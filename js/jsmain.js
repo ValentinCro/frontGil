54e53708010c5cde2203c7936d9432558cd969fd
Matthieu Coulon <Matthieu Coulon>
2016-02-01 11:42:00 +0100

    update ajout carousel

--- js/jsmain.js
+++ js/jsmain.js
@@ -160,4 +160,15 @@ function verifFormAddPost() {
     }
 }
 
+$(document).ready(function() {
 
+    $("#owl-demo").owlCarousel({
+
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+        items : 4,
+        itemsDesktop : [1199,3],
+        itemsDesktopSmall : [979,3]
+
+    });
+    });
