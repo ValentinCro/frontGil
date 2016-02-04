c833deca6f6437364e98285193f6b5c82863572b
valentinCro <valentin.crochemore1@etu.univ-rouen.fr>
2016-02-04 13:27:30 +0100

    merge

--- web/js/offlineReversi.js
+++
@@ -1,5 +0,0 @@
-
-var session = false;
-var playerColor = '#000';
-var boardColor = '#008000';
-var iaColor = '#FFFFF';
--- web/js/utilAdmin.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by valentin on 1/19/16.
- */
-$(function () {
-    $('[data-toggle="popover"]').popover()
-})
